16b8470918b1cca61992f79550858d66665dd346
NaomiSackey <naomisackey1@yahoo.com>
2023-04-20 08:34:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include<studio.h>
+#include<stdio.h>
 /**
  * main - A program that prints aline with puts function
  *
  * Return: Always 0 (Success)
- * */
+ */
 init main (void) 
 { 
 	puts ("\“ Programming is like building  a multilingual puzzle”);
