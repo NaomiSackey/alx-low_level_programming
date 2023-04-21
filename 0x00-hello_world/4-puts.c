f3713f20fa151692605771796ad75c7ad60030fd
NaomiSackey <naomisackey1@yahoo.com>
2023-04-20 17:02:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return always O (Sucess)
+ */
+int main (void)
+{
+	puts("\" Programming is like building a multilingual puzzle"/");
+	Return (O);
+}
