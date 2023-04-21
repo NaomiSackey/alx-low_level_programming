55b0d9f429de8bfbc00ab22bbeefc47d2483c535
NaomiSackey <naomisackey1@yahoo.com>
2023-04-20 22:55:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that print a line to the standard error
+ * main - A c program that prins a line to the standard error
  * Return: Always 1 (Success)
  */
 int main(void)
