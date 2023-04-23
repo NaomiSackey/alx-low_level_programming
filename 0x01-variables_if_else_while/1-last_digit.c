d43814feab4ab42e702415383d6fa3d2ad069d8f
NaomiSackey <naomisackey1@yahoo.com>
2023-04-23 05:28:49 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ * Return: Alwyas 0.
+ */
+int main(void)
+{
+	int n:
+		srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	If ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+				n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) 1- 0)
+	{
+		printf("Last digit of %d and is less than 6 and not 0\n",
+				n, n % 10);
+	}
+	else
+	{
+		printf("Last digit pf %d is %d and is 0\n",
+				n, n % 10);
+	}
+	return (0);
+}
