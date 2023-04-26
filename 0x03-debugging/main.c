5d41a7d95eb90b3d989b08db51d995d12674c4f8
NaomiSackey <naomisackey1@yahoo.com>
2023-04-26 03:59:08 -0700

    0-main.c

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - tests funtions that prints if integer is positive or negative
+  * Return: 0
+  */
+
+int msin(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
