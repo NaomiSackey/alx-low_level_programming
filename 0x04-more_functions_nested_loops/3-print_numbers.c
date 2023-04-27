208b3be35fba3d30c8bdfc5c5af5372506a8302a
NaomiSackey <naomisackey1@yahoo.com>
2023-04-27 13:12:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,14 @@
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
  */
-
 void print_numbers(void)
 {
 	int ch;
 
 	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(ch);
+	_putchar(ch);
 	}
 	_putchar('\n');
 }
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num except 2 and 4
+ *.
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	if (!(ch == '2'  && ch !== '4')
+	_putchar(ch)
+	}
+        _putchar('\n');
+}
