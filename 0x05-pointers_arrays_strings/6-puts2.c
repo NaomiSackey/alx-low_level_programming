ac3128f9f82a49ebe412831e653b3993a4608cf5
NaomiSackey <naomisackey1@yahoo.com>
2023-05-02 03:19:21 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* puts2 - func should prints every character of a string.
+* puts2 - func should print only one character of a string
 * @str: input
 * Return: print
 */
@@ -9,11 +9,8 @@
 void puts2(char *str)
 {
 int longi = 0;
-
 int t = 0;
-
 char *y = str;
-
 int o;
 
 while (*y != '\0')
@@ -28,6 +25,6 @@ if (o % 2 == 0)
 {
 _putchar(str[o]);
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
