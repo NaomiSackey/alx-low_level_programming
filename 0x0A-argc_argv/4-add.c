a0043b77929b8c3a5e61c0a29800cfc685591386
NaomiSackey <naomisackey1@yahoo.com>
2023-05-15 22:48:49 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,6 +9,7 @@
 * Return: Always 0 (Success)
 */
 int check_num(char *str)
+
 {
 /*Declaring variables*/
 unsigned int count;
@@ -32,7 +33,7 @@ return (1);
 * Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
-}
+{
 /*Declaring variables*/
 int count;
 
@@ -41,11 +42,13 @@ int str_to_int;
 int sum = 0;
 
 count = 1;
-while (count < argc) /*Goes through the whole array*/
+while (count < argc)
+/*Goes through the whole array*/
 {
 if (check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+str_to_int = atoi(argv[count]);
+/*ATOI --> convert string to int*/
 sum += str_to_int;
 }
 /*Condition if one of the number contains symbols that are not digits*/
@@ -56,6 +59,7 @@ return (1);
 }
 count++;
 }
-printf("%d\n", sum); /*print sum*/
+printf("%d\n", sum);
+/*print sum*/
 return (0);
 }
